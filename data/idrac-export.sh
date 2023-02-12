5aa041303681abd2dfddcfea60ce9eac38432f9d
TBKU <tbkurtz@gmail.com>
2023-02-12 15:47:30 -0500

    update

--- data/idrac-export.sh
+++ data/idrac-export.sh
@@ -6,11 +6,6 @@ ADDR=$(sed -n '2 p' .secrets | awk -F ',' '{print $2}')
 USER=$(sed -n '2 p' .secrets | awk -F ',' '{print $3}')
 PASS=$(sed -n '2 p' .secrets | awk -F ',' '{print $4}')
 
-echo "$HOST"
-echo "$ADDR"
-echo "$USER"
-echo "$PASS"
-
 LOG='idrac.log'
 /bin/touch "$LOG"; /bin/chmod 777 "$LOG"
 while true; do
@@ -18,7 +13,7 @@ while true; do
         /bin/bash scripts/fanspeed.sh "$HOST" "$ADDR" "$USER" "$PASS"
         /bin/bash scripts/pwr_consumption.sh "$HOST" "$ADDR" "$USER" "$PASS"
         /bin/bash scripts/temp_board.sh "$HOST" "$ADDR" "$USER" "$PASS"
-    } | tee "$LOG"
+    } &> "$LOG"
     sleep 5
 done
 exit 0
